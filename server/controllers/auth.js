c0f01cf188faa796a1815754b3c21abc0599fa56
okomojacob <okomo.code@gmail.com>
2023-01-06 12:52:08 +0300

    Login controllers  :closed_lock_with_key:

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -15,7 +15,7 @@ export const register = async (req, res) => {
       location,
       occupation,
     } = req.body;
-    
+
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
@@ -29,12 +29,31 @@ export const register = async (req, res) => {
       location,
       occupation,
       viewedProfile: Math.floor(Math.random() * 10000),
-      impressions: Math.floor(Math.random() * 10000)
+      impressions: Math.floor(Math.random() * 10000),
     });
     // Save the new user & send their nes json file to frontend.
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (err) {
-    res.status(500).json({error: err.message});
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* LOGGIN IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User doesn not exist. " });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentails. " });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+    
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
