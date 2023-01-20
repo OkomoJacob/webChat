56f178593be3953c513d8de9721c168443e4b5e8
OkomoJacob <okomo.code@gmail.com>
2023-01-20 06:14:59 +0300

    Building :hammer: user profile :pencil2:

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -1,7 +1,7 @@
 import {
   ManageAccountsOutlined,
   EditOutlined,
-  LocationOutlined,
+  LocationOnOutlined,
   WorkOtlined,
 } from "@mui/icons-material";
 import { Box, Typography, Divider, useTheme } from "@mui/material";
@@ -75,7 +75,57 @@ const UserWidget = ({ userId, picturePath }) => {
             </Typography>
             <Typography color={medium}>{friends.length} friends</Typography>
           </Box>
+          <ManageAccountsOutlined />
         </FlexBetween>
+
+        {/* SECOND ROW */}
+        <Box p="1rem 0">
+          <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+            <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+            <Typography color={medium}> {location} </Typography>
+          </Box>
+          <Box display="flex" alignItems="center" gap="1rem">
+            <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
+            <Typography color={medium}> {occupation} </Typography>
+          </Box>
+        </Box>
+
+        {/* THIRD ROW */}
+        <Box p="1rem 0">
+          <FlexBetween mb="0.5rem">
+            <Typography color={medium}>Who's viewed your profile</Typography>
+            <Typography color={main} fontWeight="500">
+              {viewedProfile}
+            </Typography>
+          </FlexBetween>
+          <FlexBetween>
+            <Typography color={medium}>Impressions of your post</Typography>
+            <Typography color={main} fontWeight="500">
+              {impressions}
+            </Typography>
+          </FlexBetween>
+        </Box>
+
+        {/* FOURTH ROW */}
+        <Box p="1rem 0">
+          <Typography fontSize="1rem" fontWeight="500" color={main} mb="1rem">
+            Social Profiles
+          </Typography>
+          <FlexBetween gap="1rem" mb="0.5rem">
+            <FlexBetween gap="1rem">
+              <img src="../assets/twitter.png" alt="twitter" />
+              <Box>
+                <Typography color={main} fontWeight="500">
+                  Twitter
+                </Typography>
+                <Typography color={medium}>Social Network</Typography>
+              </Box>
+            </FlexBetween>
+            <EditOutlined sx={{ color: main }} />
+          </FlexBetween>
+
+          
+        </Box>
       </FlexBetween>
     </WidgetWrapper>
   );
